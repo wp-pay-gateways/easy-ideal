e2fa33cb19b961645dba369c4fd77682a82b8d85
Remco Tolsma <info@remcotolsma.nl>
2015-11-12 16:03:30 +0100

    Fixed unexpected ','.

--- src/Integration.php
+++ src/Integration.php
@@ -4,7 +4,7 @@ class Pronamic_WP_Pay_Gateways_EasyIDeal_Integration {
 	public function __construct() {
 		$this->id            = 'easy-ideal';
 		$this->name          = 'Easy iDeal - iDEAL';
-		$this->url           = 'http://www.easy-ideal.com/',
+		$this->url           = 'http://www.easy-ideal.com/';
 		$this->dashboard_url = 'https://www.qantanipayments.com/backoffice/login/';
 		$this->provider      = 'easy-ideal';
 	}
