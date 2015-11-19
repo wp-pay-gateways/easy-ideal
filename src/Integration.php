83a6075c7b2decdceac6011cb39e4baa2ef9f148
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 11:44:41 +0100

    Extend from Qantas class.

--- src/Integration.php
+++ src/Integration.php
@@ -1,23 +1,11 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_EasyIDeal_Integration {
+class Pronamic_WP_Pay_Gateways_EasyIDeal_Integration extends Pronamic_WP_Pay_Gateways_Qantani_Integration {
 	public function __construct() {
 		$this->id            = 'easy-ideal';
 		$this->name          = 'Easy iDeal - iDEAL';
 		$this->url           = 'http://www.easy-ideal.com/';
 		$this->dashboard_url = 'https://www.qantanipayments.com/backoffice/login/';
 		$this->provider      = 'easy-ideal';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_Qantani_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_Qantani_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_Qantani_Gateway';
 	}
 }
