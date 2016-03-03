94be4b15e1dd977d74b8b95344c434f58c85c549
Reüel van der Steege <reuel@pronamic.nl>
2016-03-03 16:49:08 +0100

    Add product URL

--- src/Integration.php
+++ src/Integration.php
@@ -14,6 +14,7 @@ class Pronamic_WP_Pay_Gateways_EasyIDeal_Integration extends Pronamic_WP_Pay_Gat
 		$this->id            = 'easy-ideal';
 		$this->name          = 'Easy iDeal - iDEAL';
 		$this->url           = 'http://www.easy-ideal.com/';
+		$this->product_url   = 'http://www.easy-ideal.com/';
 		$this->dashboard_url = 'https://www.qantanipayments.com/backoffice/login/';
 		$this->provider      = 'easy-ideal';
 		$this->deprecated    = false;
